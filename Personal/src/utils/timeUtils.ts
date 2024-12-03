771d2e558254f7f4d904cc9d15bcf9b22b5c58a8
shinboseishiro <yndazhangg@gmail.com>
2024-12-03 19:06:44 +0700

    Delete Personal directory

--- Personal/src/utils/timeUtils.ts
+++
@@ -1,9 +0,0 @@
-export const getGreeting = (): string => {
-  // Use Asia/Jakarta timezone
-  const hour = new Date().getHours();
-  
-  if (hour >= 5 && hour < 12) return 'Selamat Pagi';
-  if (hour >= 12 && hour < 15) return 'Selamat Siang';
-  if (hour >= 15 && hour < 18) return 'Selamat Sore';
-  return 'Selamat Malam';
-};
